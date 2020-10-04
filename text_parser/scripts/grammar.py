5a303a38763f8c2da8744b662b4dcd0374104e8e
daihaminkey <daihaminkey@icloud.com>
2020-10-04 04:50:08 +0300

    - .jar-fail moved to root directiry - namedtuple and args replaced with data classes

--- text_parser/scripts/grammar.py
+++ text_parser/scripts/grammar.py
@@ -1,15 +1,16 @@
 from pathlib import Path
 
+from atlr4_compiler.compile_options import GrammarConfig
 from text_parser.utils import calculate_hash
 
 
 class Grammar:
-    def __init__(self, path: Path, root_rule: str):
-        if str(path.absolute())[-3:] != '.g4':
-            raise NameError(path.absolute())
+    def __init__(self, config: GrammarConfig):
+        if str(config.grammar_file)[-3:] != '.g4':
+            raise NameError(config.grammar_file)
 
-        self.path = path
-        self.name = path.name
+        self.path = config.grammar_file
+        self.name = config.grammar_file.name
         self.hash = calculate_hash(self.path)
 
-        self.root_rule = root_rule
+        self.root_rule = config.root_grammar_rule
